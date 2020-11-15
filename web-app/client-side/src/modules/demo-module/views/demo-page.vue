3ee77b6649e7ee3f15ca4426944faba93dda66c0
Omar Sabry <omar.sabry@AssetDev.com>
2020-11-15 18:06:00 +0200

    added demo module

---
+++ web-app/client-side/src/modules/demo-module/views/demo-page.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <TheNavbar />
+        <v-app>
+            <v-container>
+                <v-row>
+                    <v-col>
+                        <v-file-input
+                            show-size
+                            outlined
+                            counter
+                            label="Input File" v-model="file"
+                        ></v-file-input>
+                    </v-col>
+                    <v-col>
+                        <v-btn @click="upload">Upload</v-btn>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-app>    
+    </div>
+</template>
+
+<script>
+import TheNavbar from '../../core-module/components/the-nav-bar'
+
+export default {
+    name: 'DemoPage',
+    data() {
+        return {
+            file: null
+        }
+    },
+    components: {
+        TheNavbar,
+    },
+    methods: {
+        upload () {
+            console.log(this.file);
+        }
+    }
+}
+</script>
