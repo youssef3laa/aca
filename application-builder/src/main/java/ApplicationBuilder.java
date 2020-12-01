7d2f38d2221134a67c376a133b1e1f8424763eac
Karim <karim.omaya.elfaham@gmail.com>
2020-12-01 16:26:36 +0400

    Update Application Builder

---
+++ application-builder/src/main/java/ApplicationBuilder.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by karim on 12/1/20.
+ */
+@SpringBootApplication
+public class ApplicationBuilder {
+
+    public static void main(String[] args) {
+
+
+    }
+}
