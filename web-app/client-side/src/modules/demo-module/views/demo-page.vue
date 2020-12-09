ae1fbef3392ae4776634620fbfff67f068f1236f
Ahmed Ezzat <ahmed.salama456@gmail.com>
2020-12-09 13:09:08 +0200

    fixing IE Error

--- web-app/client-side/src/modules/demo-module/views/demo-page.vue
+++ web-app/client-side/src/modules/demo-module/views/demo-page.vue
@@ -1,43 +1,39 @@
 <template>
-    <div>
-        <TheNavbar />
-        <v-app>
-            <v-container>
-                <v-row>
-                    <v-col>
-                        <v-file-input
-                            show-size
-                            outlined
-                            counter
-                            label="Input File" v-model="file"
-                        ></v-file-input>
-                    </v-col>
-                    <v-col>
-                        <v-btn @click="upload">Upload</v-btn>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-app>    
-    </div>
+  <div>
+    <v-app>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-file-input
+                v-model="file"
+                counter="1"
+                label="Input File"
+                outlined show-size
+            ></v-file-input>
+          </v-col>
+          <v-col>
+            <v-btn @click="upload">Upload</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import TheNavbar from '../../core-module/components/the-nav-bar'
 
 export default {
-    name: 'DemoPage',
-    data() {
-        return {
-            file: null
-        }
-    },
-    components: {
-        TheNavbar,
-    },
-    methods: {
-        upload () {
-            console.log(this.file);
-        }
+  name: 'DemoPage',
+  data() {
+    return {
+      file: null
     }
+  },
+  components: {},
+  methods: {
+    upload() {
+      console.log(this.file);
+    }
+  }
 }
 </script>
