08879f11eee4f3e8b16aa08e4047e2e33befe028
Alley El-Dorry <eldorry3@gmail.com>
2020-12-30 12:09:38 +0200

    created new mixin for history module

--- web-app/client-side/src/mixins/formPageMixin.js
+++ web-app/client-side/src/mixins/formPageMixin.js
@@ -48,23 +48,6 @@ export default {
             })
             .catch((error) => console.error(error));
         },
-        async getHistoryByProcessNameAndEntityId(processName, entityId){
-            try{
-                var response  =  await http.get("/history/"+processName+"/"+entityId);
-                return response.data.json();
-            }
-            catch(error){
-                console.error(error);
-            }
-        },
-        async setHistory(data){
-            try{
-                var response = await http.post("/history",data);
-                console.log(response.data);
-            }
-            catch (err){
-                console.error(err);
-            }
-        }
+
         }
     }
---
+++ web-app/client-side/src/modules/history-module/mixin/historyPageMixin.js
@@ -0,0 +1,25 @@
+import http from "../modules/core-module/services/http";
+
+export default{
+
+    methods:{
+        async getHistoryByProcessNameAndEntityId(processName, entityId){
+            try{
+                var response  =  await http.get("/history/"+processName+"/"+entityId);
+                return response.data.json();
+            }
+            catch(error){
+                console.error(error);
+            }
+        },
+        async setHistory(data){
+            try{
+                var response = await http.post("/history",data);
+                console.log(response.data);
+            }
+            catch (err){
+                console.error(err);
+            }
+        }
+}
+}
