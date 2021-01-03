9f4d05e8c37a9f0d862e528ccfe85fac17b7a6ff
Alley El-Dorry <eldorry3@gmail.com>
2021-01-03 18:20:57 +0200

    fixed error

--- web-app/client-side/src/modules/correspondence-data-module/mixin/correspondenceMixin.js
+++ web-app/client-side/src/modules/correspondence-data-module/mixin/correspondenceMixin.js
@@ -1,20 +1,20 @@
-import http from "../../core-module/services/http";
+// // import http from "../../core-module/services/http";
 
-export default {
+// export default {
 
-    methods:{
-        async getMemoData(memoName){
-        //     try{
-        //         var response  =  await http.get("memo/"+memoName);
-        //         return response.data.data;
-        //     }
-        //     catch(error){
-        //         console.error(error);
-        //     }
-        // },
-        return response = [
-            {mem:''}
-        ]
+//     methods:{
+//         async getMemoData(memoName){
+//         //     try{
+//         //         var response  =  await http.get("memo/"+memoName);
+//         //         return response.data.data;
+//         //     }
+//         //     catch(error){
+//         //         console.error(error);
+//         //     }
+//         // },
+//         return response = [
+//             {mem:''}
+//         ]
 
-}
-}}
+// }
+// }}
