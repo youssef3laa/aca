6d78dbc724b560ec79bd9b0a139ef957aef1be3e
Ahmed Ezzat <ahmed.salama456@gmail.com>
2021-01-12 13:55:41 +0200

    fixing pointer events in brava iframe

--- web-app/client-side/src/modules/process-module/views/process-step-early.vue
+++ web-app/client-side/src/modules/process-module/views/process-step-early.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-container>
-        <AppBuilder ref="appBuilder" :app="app"/>
-    </v-container>
+  <v-container>
+    <AppBuilder ref="appBuilder" :app="app"/>
+  </v-container>
 </template>
 
 <script>
@@ -22,132 +22,120 @@ export default {
       console.log("TaskData", this.taskData);
       this.inputSchema = this.taskData.TaskData.ApplicationData.ACA_ProcessRouting_InputSchemaFragment;
 
-                this.loadForm("process-stepEarly", this.fillForm);
-            },
-            fillForm: function () {
-                this.$refs.appBuilder.disableSection("section1");
-                let entityName = this.inputSchema.entityName;
-                let entityId = this.inputSchema.entityId;
-                this.readEntity(entityName, entityId)
-                    .then(async (response) => {
-                        response = JSON.parse(response.data.data);
+      this.loadForm("process-stepEarly", this.fillForm);
+    },
+    fillForm: function () {
+      this.$refs.appBuilder.disableSection("section1");
+      let entityName = this.inputSchema.entityName;
+      let entityId = this.inputSchema.entityId;
+      this.readEntity(entityName, entityId)
+          .then(async (response) => {
+            response = JSON.parse(response.data.data);
 
-                        this.$refs.appBuilder.setModelData("form1", {
-                            stepId: this.inputSchema.stepId,
-                            notes: response.notes,
-                            receiver: {
-                                url: this.inputSchema.roleFilter,
-                                list: [],
-                                value: ""
-                            },
-                            requestDate: response.requestDate.split("Z")[0],
-                        });
-                        this.approvalsHistoryResponse = await this.getHistoryByProcessNameAndEntityId(
-                            this.inputSchema.process,
-                            this.inputSchema.entityId
-                        );
-                        console.log(this.approvalsHistoryResponse);
-                        this.$refs.appBuilder.setModelData("historyTable", {
-                            taskTable: {
-                                headers: [
-                                    {
-                                        text: "القرار",
-                                        align: "start",
-                                        value: "decision",
-                                    },
-                                    {
-                                        text: "الاسم",
-                                        align: "start",
-                                        value: "userCN",
-                                    },
-                                    {
-                                        text: "التاريخ",
-                                        value: "approvalDate",
-                                    },
-                                ],
-                                data: this.approvalsHistoryResponse,
-                                search: "",
-                            },
-                        });
-
-                        console.log("response", response);
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                    });
-            },
-        },
-        async created() {
-            this.taskId = this.$route.params.taskId;
-            this.claimTask(this.taskId);
-            this.getTaskData(this.taskId);
-
-            this.$observable.subscribe("complete-step", () => {
-                let model = this.$refs.appBuilder.getModelData("form1");
-                // if (!model._valid){
-                //   //@TODO show warning
-                //   return;
-                // }
-                let approvalModel = this.$refs.appBuilder.getModelData("ApprovalForm");
-
-                var data = {
-                    taskId: this.taskId,
-                    entityId: this.inputSchema.entityId,
-                    stepId: this.inputSchema.stepId,
-                    process: this.inputSchema.process,
-                    parentHistoryId: this.inputSchema.parentHistoryId,
-
-                    code: approvalModel.approval.decision,
-                    assignedCN: model.receiver.value.value,
-                    decision: approvalModel.approval.decision,
-                    comment: approvalModel.approval.comment,
-                };
-                this.completeStep(data);
+            this.$refs.appBuilder.setModelData("form1", {
+              stepId: this.inputSchema.stepId,
+              notes: response.notes,
+              receiver: {
+                url: this.inputSchema.roleFilter,
+                list: [],
+                value: ""
+              },
+              requestDate: response.requestDate.split("Z")[0],
+            });
+            this.approvalsHistoryResponse = await this.getHistoryByProcessNameAndEntityId(
+                this.inputSchema.process,
+                this.inputSchema.entityId
+            );
+            console.log(this.approvalsHistoryResponse);
+            this.$refs.appBuilder.setModelData("historyTable", {
+              taskTable: {
+                headers: [
+                  {
+                    text: "القرار",
+                    align: "start",
+                    value: "decision",
+                  },
+                  {
+                    text: "الاسم",
+                    align: "start",
+                    value: "userCN",
+                  },
+                  {
+                    text: "التاريخ",
+                    value: "approvalDate",
+                  },
+                ],
+                data: this.approvalsHistoryResponse,
+                search: "",
+              },
             });
 
-            this.$observable.subscribe('open-file-brava', async (fileId) => {
-                this.$observable.fire('file-component-skeleton', true)
+            console.log("response", response);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    },
+  },
+  async created() {
+    this.taskId = this.$route.params.taskId;
+    this.claimTask(this.taskId);
+    this.getTaskData(this.taskId);
 
-                console.log(fileId);
-                let userToken;
-                try {
-                  userToken = await Http.post("http://45.240.63.94:8081/otdsws/rest/authentication/credentials", {
-                    "userName": "admin",
-                    "password": "Asset99a",
-                    "ticketType": "OTDSTICKET"
-                  });
-                  this.$refs.appBuilder.getModelData('iframeObj')['iframeObj']['src'] =
-                      'http://45.240.63.94/otcs/cs.exe?func=brava.bravaviewer&nodeid=' + fileId + '&viewType=1&OTDSTicket=' + userToken.data.ticket;
-                  console.log(userToken);
-                    //this.$observable.fire('file-component-skeleton', false)
-                    userToken = await Http.post("http://appworks-dev:8080/otdsws/rest/authentication/credentials", {
-                        "userName": "admin",
-                        "password": "Asset99a",
-                        "ticketType": "OTDSTICKET"
-                    });
-                    this.$refs.appBuilder.getModelData('iframeObj')['iframeObj']['src'] =
-                        'http://appworks-dev/otcs/cs.exe?func=brava.bravaviewer&nodeid=' + fileId + '&viewType=1&OTDSTicket=' + userToken.data.ticket;
-                    console.log(userToken);
-                    // this.$observable.fire('file-component-skeleton', false)
+    this.$observable.subscribe("complete-step", () => {
+      let model = this.$refs.appBuilder.getModelData("form1");
+      // if (!model._valid){
+      //   //@TODO show warning
+      //   return;
+      // }
+      let approvalModel = this.$refs.appBuilder.getModelData("ApprovalForm");
 
-                } catch (e) {
-                    console.log(e);
-                }
+      var data = {
+        taskId: this.taskId,
+        entityId: this.inputSchema.entityId,
+        stepId: this.inputSchema.stepId,
+        process: this.inputSchema.process,
+        parentHistoryId: this.inputSchema.parentHistoryId,
 
+        code: approvalModel.approval.decision,
+        assignedCN: model.receiver.value.value,
+        decision: approvalModel.approval.decision,
+        comment: approvalModel.approval.comment,
+      };
+      this.completeStep(data);
+    });
 
-            });
+    this.$observable.subscribe('open-file-brava', async (fileId) => {
+      this.$observable.fire('file-component-skeleton', true)
 
-        },
+      console.log(fileId);
+      let userToken;
+      try {
+        userToken = await Http.post("http://45.240.63.94:8081/otdsws/rest/authentication/credentials", {
+          "userName": "admin",
+          "password": "Asset99a",
+          "ticketType": "OTDSTICKET"
+        });
+        this.$refs.appBuilder.getModelData('iframeObj')['iframeObj']['src'] =
+            'http://45.240.63.94/otcs/cs.exe?func=brava.bravaviewer&nodeid=' + fileId + '&viewType=1&OTDSTicket=' + userToken.data.ticket;
+        console.log(userToken);
+        //this.$observable.fire('file-component-skeleton', false)
+      } catch (e) {
+        console.log(e);
+      }
+    });
 
-        data() {
-            return {
-                taskId: "",
-                taskData: {},
-                inputSchema: {},
-                app: {},
-                model: {},
-                approvalsHistoryResponse: ''
-            };
-        },
+  },
+
+  data() {
+    return {
+      taskId: "",
+      taskData: {},
+      inputSchema: {},
+      app: {},
+      model: {},
+      approvalsHistoryResponse: ''
     };
+  },
+};
 </script>
