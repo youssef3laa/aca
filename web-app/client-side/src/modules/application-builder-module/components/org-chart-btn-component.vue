462bd8a49d328b8a5af61c9cab1568fe7cfebba3
Alley El-Dorry <eldorry3@gmail.com>
2021-01-21 19:14:57 +0200

    Created tasks-list, side-bar, org-chart-btn, advanced-search components to be used in the landing page

---
+++ web-app/client-side/src/modules/application-builder-module/components/org-chart-btn-component.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row org-chart-wrapper">
+ 
+      <div class="org-icon-wrapper">
+        <i class="fas fa-sitemap"></i>
+      </div>
+      <span>الهيكلي<br>المخطط</span>
+    </div>
+
+</template>
+
+<script>
+export default {
+  props: ["val", "field"],
+
+
+};
+</script>
+
+<style>
+
+.org-icon-wrapper {
+  display: flex;
+  width: 28px;
+  height: 28px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20%;
+  background: #F37121;
+  border-radius: 5px;
+}
+.org-icon-wrapper i {
+  color: white;
+}
+
+.org-chart-wrapper {
+  display: flex;
+  height: 80px;
+  background: white;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 10px ;
+  align-items: center !important;
+  justify-content: space-around;
+}
+</style>
---
+++ web-app/client-side/src/modules/application-builder-module/components/sidebar-component.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="sidebar-wrapper">
+    <div class="sidebar-item">
+      <span> <span> الصادر</span> 34</span>
+      <i class="far fa-edit"></i>
+
+    </div>
+    <div class="sidebar-item">
+      <span> <span> الوارد</span> 34</span>
+      <i class="far fa-edit"></i>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.sidebar-wrapper {
+  display: flex;
+  flex-direction: column;
+  background: white;
+height: 100%;
+
+  align-items: center !important;
+  justify-content: center;
+}
+.sidebar-item {
+  display: flex;
+  align-items: center;
+  background-color: rgba(7, 104, 159, 0.05);
+  margin: 10px;
+  height: 40px;
+  width: 80%;
+  padding: 10px;
+  justify-content: center;
+  border-radius: 5px;
+  color: #0278ae;
+}
+
+.sidebar-item i {
+  margin-left: 10px;
+}
+</style>
