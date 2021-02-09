219a00f41fcea6399ec27a9dc6d796e33c0f361d
mohamed-taha94 <68019194+mohamed-taha94@users.noreply.github.com>
2021-02-09 14:54:42 +0200

    Register Alert component and confirmation messages globally.

--- web-app/client-side/src/main.js
+++ web-app/client-side/src/main.js
@@ -6,21 +6,26 @@ import i18n from './modules/core-module/lib/i18n'
 import router from './router'
 import { ValidationProvider } from 'vee-validate'
 import axios from '../node_modules/axios'
-import VueSignaturePad  from 'vue-signature-pad'
-
+import VueSignaturePad from 'vue-signature-pad'
+import ConfirmDialogue from '../src/modules/application-builder-module/components/ConfirmDialogue'
+import AlertComponent from '../src/modules/application-builder-module/components/alertComponent'
 Vue.use(VueSignaturePad);
 
 Vue.component('ValidationProvider', ValidationProvider)
 Vue.component('axios', () => {})
+Vue.component('ConfirmDialogue', ConfirmDialogue)
+Vue.component('AlertComponent', AlertComponent)
 
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
-  i18n,
-  router,
-  axios,
-  observable,
-  VueSignaturePad,
-  render: (h) => h(App),
+    vuetify,
+    i18n,
+    router,
+    axios,
+    observable,
+    ConfirmDialogue,
+    AlertComponent,
+    VueSignaturePad,
+    render: (h) => h(App),
 }).$mount('#app')
