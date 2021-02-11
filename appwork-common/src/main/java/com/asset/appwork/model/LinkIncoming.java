54f58e42bffdd08ed867d91a34772938a053ceb5
Ahmed Ezzat <ahmed.salama456@gmail.com>
2021-02-11 14:17:20 +0200

    work on link-incoming process

--- appwork-common/src/main/java/com/asset/appwork/model/LinkIncoming.java
+++ appwork-common/src/main/java/com/asset/appwork/model/LinkIncoming.java
@@ -19,6 +19,8 @@ public class LinkIncoming {
     private String sourceIncomingId;
     private String targetIncomingId;
     private String requestEntityId;
+    private String sourceRequestId;
+    private String targetRequestId;
 
     public String toString() {
         return SystemUtil.writeObjectIntoString(this).replace(",\"id\":null", "");
