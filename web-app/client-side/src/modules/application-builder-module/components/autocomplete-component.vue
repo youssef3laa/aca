6ba992653e539d5568fb820cf502c53908e9dea9
Omar Sabry <omar.adell96@gmail.com>
2021-02-16 14:35:52 +0200

    update data table

--- web-app/client-side/src/modules/application-builder-module/components/autocomplete-component.vue
+++ web-app/client-side/src/modules/application-builder-module/components/autocomplete-component.vue
@@ -57,16 +57,16 @@ export default {
             let obj = {}
             if (element['cn']) {
               obj = {
-                name: element['name_ar'],
+                name: element['nameAr'],
                 value: element['cn'],
-                text: element['name_ar'],
+                text: element['nameAr'],
                 code: element['groupCode'],
                 object: element,
               }
             } else if (element['unitCode']) {
               obj = {
                 value: element['unitCode'],
-                text: element['name_ar'],
+                text: element['nameAr'],
                 object: element,
               }
             } else {
