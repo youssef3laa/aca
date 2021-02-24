2bb995eeeef021763e18d4632cbc531b2fb65e35
MostafaRefaii <mostafa.refaii@outlook.com>
2021-02-24 17:08:43 +0200

    update navbar & barcode component

--- web-app/client-side/src/modules/application-builder-module/builders/form-builder.vue
+++ web-app/client-side/src/modules/application-builder-module/builders/form-builder.vue
@@ -66,6 +66,7 @@ import SaveProcessComponent from '../components/save-process-component'
 import SecretaryViewSignaturesComponent from '../components/secretary-view-signatures-component'
 import InboxComponent from '../components/inbox-component'
 import ImageComponent from "../components/image-component"
+import BarcodeComponent from "../components/barcode-component"
 
 export default {
   name: 'FormBuilder',
@@ -101,7 +102,8 @@ export default {
     SaveProcessComponent,
     SecretaryViewSignaturesComponent,
     InboxComponent,
-    ImageComponent
+    ImageComponent,
+    BarcodeComponent,
   },
   data() {
     return {
