58f635c44c86d946bb3043569994f547940f3906
Omar Sabry <omar.adell96@gmail.com>
2021-03-03 11:20:50 +0200

    fix in inbox

--- web-app/client-side/src/modules/application-builder-module/components/office-group-head-inbox.vue
+++ web-app/client-side/src/modules/application-builder-module/components/office-group-head-inbox.vue
@@ -123,11 +123,7 @@ export default {
         let fromCertifications = [];
 
         for (let key in data) {
-          if(!data[key].TaskData.ApplicationData.ACA_ProcessRouting_InputSchemaFragment.caseType) {
-            fromAdmins.push(data[key])
-            continue
-          }
-          switch (data[key].TaskData.ApplicationData.ACA_ProcessRouting_InputSchemaFragment.caseType) {
+          switch (data[key].TaskData.ApplicationData?.ACA_ProcessRouting_InputSchemaFragment?.caseType) {
             case "certificationTechnicalOffice":
               fromCertifications.push(data[key]);
               break;
