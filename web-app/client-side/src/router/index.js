b392d8e133d9a520226e16918d7f85935b18f71b
mohamed-taha94 <68019194+mohamed-taha94@users.noreply.github.com>
2021-03-09 15:19:07 +0200

    Implements Dynamic Notifications and adds CKeditor Rich text component.

--- web-app/client-side/src/router/index.js
+++ web-app/client-side/src/router/index.js
@@ -16,6 +16,7 @@ import observable from "../modules/core-module/lib/vue-sub-lib";
 import linkingRoutes from '../modules/link-incoming-module/router/router'
 import basicSearchRoutes from '../modules/basicSearch-module/router/router'
 import annualGoalsRoutes from '../modules/goals-module/router/router'
+import DynamicNotification from '../modules/dynamic-notification-module/router/router'
 
 
 Vue.use(VueRouter)
@@ -51,6 +52,7 @@ allRoutes = allRoutes.concat(
     linkingRoutes,
     EscalationRoutes,
     annualGoalsRoutes,
+    DynamicNotification,
     basicSearchRoutes, {
         path: '/',
         name: 'Home',
