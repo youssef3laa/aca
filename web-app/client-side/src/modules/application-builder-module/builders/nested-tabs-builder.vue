be8d04b261599f98ac123ad03ae770051481bcf4
mohamed-taha94 <68019194+mohamed-taha94@users.noreply.github.com>
2021-03-11 15:05:59 +0200

    Adds Nested Tabs arrow (next-previous)

--- web-app/client-side/src/modules/application-builder-module/builders/nested-tabs-builder.vue
+++ web-app/client-side/src/modules/application-builder-module/builders/nested-tabs-builder.vue
@@ -5,15 +5,31 @@
     border-radius:6px;border-radius-top-righ:6px; border-radius-top-left:6px;
     margin: 15px; padding: 5px;"
     >
-      <button
-        style="padding:15px;"
-        v-for="tab in tabs"
-        :key="tab.id"
-        @click="selectedTab(tab)"
-        :class="['tab-btn', { active: selected === tab }]"
-      >
-        {{ $t(tab.name) }}
-      </button>
+      <v-row>
+        <v-col :cols="10" id="tabs-list">
+          <button
+            v-ripple
+            style="padding:15px;margin-right: 15px;"
+            v-for="tab in tabs"
+            :key="tab.id"
+            @click="selectedTab(tab)"
+            :class="['tab-btn', { active: selected === tab }]"
+            :cols="3"
+          >
+            {{ $t(tab.name) }}
+          </button>
+        </v-col>
+        <v-col :cols="2">
+          <div class="scroller-wrapper" style="float:left;">
+            <v-btn class="scroller" @click="scroll('right', 'tabs-list')">
+              <i class="fas fa-chevron-right"></i>
+            </v-btn>
+            <v-btn class="scroller" @click="scroll('left', 'tabs-list')">
+              <i class="fas fa-chevron-left"></i>
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
     </span>
   </v-card>
 </template>
@@ -41,6 +57,26 @@ export default {
       }
       console.log('Nested Tab Selected', tab)
     },
+    scroll(direction, content) {
+      let scrollPixels = 180
+      if (direction == 'left') scrollPixels = -scrollPixels
+
+      console.log(scrollPixels)
+      const element = document.getElementById(content)
+      // element.animate({scrollLeft: '=-300'},1000);
+      var scroll = scrollPixels / 10
+
+      var scrolled = 0
+      const interval = setInterval(() => {
+        element.scrollLeft += scroll
+        scrolled += scroll
+        console.log(scrolled)
+        console.log(scrollPixels)
+        if (Math.round(scrolled) == Math.round(scrollPixels)) {
+          clearInterval(interval)
+        }
+      }, 20)
+    },
   },
   mounted() {
     console.log(this.tabs)
@@ -57,13 +93,32 @@ export default {
 
 <style scoped>
 .tab-btn {
- outline: none;
+  outline: none;
 }
 .active {
   /* border-bottom: 3px solid #0278ae !important; */
-  border: 1px solid white;
-  border-radius: 6px;
-  background-color: white;
-  color: #0278ae;
+  border: 1px solid white !important;
+  border-radius: 6px !important;
+  background-color: white !important;
+  color: #0278ae !important;
+}
+button.tab-btn.active {
+  color: #0278ae !important;
+}
+#tabs-list {
+  overflow-x: hidden;
+  overflow-y: hidden;
+  display: inline-flex;
+}
+.scroller {
+  min-width: 34px !important;
+  width: 34px !important;
+  height: 34px !important;
+  text-indent: 0em !important;
+  margin-left: 5px;
+  background-color: transparent !important;
+  border: 1px solid #07689f;
+  color: #07689f !important;
+  box-shadow: none !important;
 }
 </style>
